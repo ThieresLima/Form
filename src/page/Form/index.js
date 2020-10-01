a6b6e6202881529b274409b2ee695f601fa58d31
Thieres Lima <thieresllima@gmail.com>
2020-10-01 15:02:09 -0300

    form

--- src/page/Form/index.js
+++ src/page/Form/index.js
@@ -1,12 +1,60 @@
 import React from 'react';
-import { Container, Main, Image } from './styles';
+import {
+    Container,
+    Main,
+    Image,
+    ContainerLogin,
+    Login,
+    LoginText,
+    LoginInput,
+    Icon,
+    LoginContainer,
+    Text,
+    TextSpan,
+    CreateAcc,
+    CreateAccContainer
+} from './styles';
+
 import img from '../../img/photo.jpeg';
+
+import { MdEmail, MdLock, MdArrowForward } from 'react-icons/md';
 
 function Form() {
     return (
         <Container>
             <Main>
                 <Image src={img} />
+
+                <ContainerLogin>
+                    <LoginText>Member Login</LoginText>
+
+                    <Login>
+                        <LoginInput type="text" placeholder="Email" />
+                        <Icon>
+                            <MdEmail color="#666666" />
+                        </Icon>
+                    </Login>
+
+                    <Login>
+                        <LoginInput type="text" placeholder="Password" />
+                        <Icon>
+                            <MdLock color="#666666" />
+                        </Icon>
+                    </Login>
+
+
+                    <LoginContainer>
+                        <Text>Login</Text>
+                    </LoginContainer>
+
+                    <TextSpan>Forgot Username / Password? </TextSpan>
+
+                    <CreateAccContainer>
+                        <CreateAcc>Create your Account</CreateAcc>
+                        <MdArrowForward />
+                    </CreateAccContainer>
+
+                </ContainerLogin>
             </Main>
         </Container>
     );
--- src/page/Form/styles.js
+++ src/page/Form/styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
     display: flex;
     background: linear-gradient( to right, rgba(39, 100, 154), rgb(137, 26, 26));
-    max-width: 100%;
+    max-width: 100vw;
     height: 750px;
     justify-content: center;
 `;
@@ -14,12 +14,95 @@ export const Main = styled.div`
     align-items: center;
     width: 960px;
     height: 650px;
-    margin-top: 14px;
+    margin-top: 20px;
     border-radius: 5px;
 `;
 
 export const Image = styled.img`
-    width: 550px;
+    width: 500px;
     height: 650px;
     border-radius: 5px;
+`;
+
+export const ContainerLogin = styled.div`
+    width: 470px;
+    height: 420px;
+    text-align: center;
+`;
+
+export const Login = styled.div`
+    display: flex;
+    width: 300px;
+    flex-direction: column;
+    position: relative;
+    margin: 0 auto;
+    padding: 5px;
+`;
+
+export const LoginText = styled.h1`
+    font-family: sans-serif;
+    padding-bottom: 50px;
+    text-align: center;
+    color: #333333;
+    font-size: 28px;
+`;
+
+export const LoginInput = styled.input`
+    width: 300px;
+    color: #938f8f;
+    font-weight: bold;
+    font-size: 14px;
+    border-radius: 30px;
+    padding: 18px;
+    text-indent: 48px;
+    border: 0 none;
+    outline: 0;
+    background-color: #E6E6E6;
+`;
+
+export const Icon = styled.span`
+    position: absolute;
+    padding: 18px;
+    margin-left: 16px;
+`;
+
+export const LoginContainer = styled.div`
+    margin-top: 30px;
+    width: 300px;
+    padding: 18px;
+    margin-left: 87px;
+    background: linear-gradient( to right, rgba(39, 100, 154), rgb(137, 26, 26));
+    border-radius: 30px;
+`;
+
+export const Text = styled.p`
+    font-family: sans-serif;
+    text-transform: uppercase;
+    color: #FFF;
+    font-size: 16;
+    font-weight: bold;
+`;
+
+export const TextSpan = styled.p`
+    font-family: sans-serif;
+    color: #666666;
+    font-size: 14px;
+    font-weight: bold;
+    padding: 14px;
+`;
+
+export const CreateAccContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    padding: 20px;
+    margin-top: 80px;
+`;
+
+export const CreateAcc = styled.p`
+    font-family: sans-serif;
+    color: #666666;
+    font-size: 14px;
+    font-weight: bold;
+    padding-right: 3px;
 `;
